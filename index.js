ae4c2ba894b517ebc0a72bdc317fc151fbbb6e35
Chris C <christopher.cruzcosa@gmail.com>
2020-10-13 21:21:04 -0700

    added first three menu options

--- index.js
+++ index.js
@@ -16,13 +16,22 @@ var connection = mysql.createConnection({
   database: "lpc_search_lightDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
+  console.log(`
+  
+Welcome to the le Phare Corporation's Human Resources Database
+  
+We want to be of help to all members of our corporate family.
+
+Remember, like our corporate motto says, 'We See You!'
+
+`)
   start();
 });
 
-function start () {
-    inquirer
+function start() {
+  inquirer
     .prompt({
       name: "action",
       type: "list",
@@ -38,27 +47,185 @@ function start () {
         "exit"
       ]
     })
-    .then(function(answer) {
+    .then(function (answer) {
       switch (answer.action) {
-      case "Find songs by artist":
-        artistSearch();
-        break;
+        case "View All Employees":
+          viewAllEmployees();
+          break;
 
-      case "Find all artists who appear more than once":
-        multiSearch();
-        break;
+        case "View All Employees by Department":
+          viewEmployeesByDepartment();
+          break;
 
-      case "Find data within a specific range":
-        rangeSearch();
-        break;
+        case "View All Employees by Manager":
+          viewEmployeesByManager();
+          break;
 
-      case "Search for a specific song":
-        songSearch();
-        break;
+        case "Add Employee":
+          // songSearch();
+          break;
 
-      case "exit":
-        connection.end();
-        break;
+
+        case "Update Employee Role":
+          // songSearch();
+          break;
+
+
+        case "Update Employee Manager":
+          // songSearch();
+          break;
+
+        case "exit":
+          connection.end();
+          break;
       }
     });
+};
+
+function viewAllEmployees() {
+  return connection.query(
+    `Select subordinates.id
+    ,subordinates.first_name
+    ,subordinates.last_name
+    , role.title as 'job title'
+    , department.name as department
+    , role.salary
+    , concat(managers.first_name,' ', managers.last_name) as manager 
+    from employee as subordinates
+    left join employee as managers
+    on subordinates.manager_id = managers.id
+    inner join role
+    on subordinates.role_id = role.id
+    inner join department
+    on department.id = role.department_id;`,
+    (err, results) => {
+      if (err) {
+        throw err;
+      };
+      console.log("");
+      console.log("");
+      console.table(results);
+      console.log("");
+      console.log("");
+      return start();
+    });
+};
+
+function viewEmployeesByDepartment() {
+  return connection.query("SELECT department.name, department.id FROM department", (err, results) => {
+    if (err) {
+      throw err;
+    }
+    const departmentNames = results.map((row) => row.name);
+    // once you have the items, prompt the user for which they'd like to bid on
+    return inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: departmentNames,
+          message: "Which department would you like to view?",
+        }
+      ])
+      .then((answer) => {
+        const chosenDepartment = results.find(
+          (row) => row.name === answer.choice
+        );
+
+        return connection.query(
+          `Select 
+          subordinates.id
+          ,subordinates.first_name
+          ,subordinates.last_name
+          ,role.title as 'job title'
+          , department.name as department
+          , role.salary
+          , concat(managers.first_name,' ', managers.last_name) as manager 
+          from department 
+          right join role 
+          on role.department_id = department.id
+          left join employee as subordinates
+          on subordinates.role_id = role.id
+          left join employee as managers
+          on subordinates.manager_id = managers.id
+          where department.id = ?
+          order by salary desc;`,
+
+          chosenDepartment.id,
+          (err, results) => {
+            if (err) {
+              throw err;
+            }
+            console.log("");
+            console.log("");
+            console.table(results);
+            console.log("");
+            console.log("");
+            return start();
+          }
+        );
+      });
+  });
+};
+
+function viewEmployeesByManager() {
+  return connection.query(`select distinct 
+  concat(managers.first_name,' ', managers.last_name) as fullName
+  , managers.id
+  from employee as subordinates
+  left join employee as managers
+  on managers.id = subordinates.manager_id
+  where subordinates.manager_id <> "" `, (err, results) => {
+    if (err) {
+      throw err;
+    };
+    console.log(results)
+    const managerNames = results.map((row) => row.fullName);
+
+    return inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices: managerNames,
+          message: "Which manager would you like to view?",
+        }
+      ])
+      .then((answer) => {
+        const chosenManager = results.find(
+          (row) => row.fullName === answer.choice
+        );
+
+        return connection.query(
+          `Select 
+          subordinates.id
+          ,subordinates.first_name
+          ,subordinates.last_name
+           ,role.title as 'job title'
+          , department.name as department
+          , role.salary
+          , concat(managers.first_name,' ', managers.last_name) as manager 
+          from employee as managers
+          right join employee as subordinates
+          on subordinates.manager_id = managers.id
+          left join role
+          on subordinates.role_id = role.id
+          left join department
+          on department.id = role.department_id
+          where managers.id = ?
+          order by role.salary desc;`, chosenManager.id,
+          (err, results) => {
+            if (err) {
+              throw err;
+            }
+            console.log("");
+            console.log("");
+            console.table(results);
+            console.log("");
+            console.log("");
+            return start();
+          }
+        );
+      });
+  });
 };
--- lpc_search_lightDB.sql
+++ lpc_search_lightDB.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS lpc_search_lightDB;
+-- DROP DATABASE IF EXISTS lpc_search_lightDB;
 
-CREATE DATABASE lpc_search_lightDB;
+-- CREATE DATABASE lpc_search_lightDB;
 
 use lpc_search_lightDB;
 
@@ -13,15 +13,109 @@ create table role(
 id int PRIMARY KEY auto_increment,
 title varchar(30) not null,
 salary decimal (10,2) not null,
-deparment_id int
+department_id int,
+foreign key (department_id) references department (id)
 );
 
 create table employee(
 id int PRIMARY KEY auto_increment,
 first_name varchar(30) not null,
 last_name varchar(30) not null,
-deparment_id int,
-manager_id int
+role_id int,
+manager_id int,
+foreign key (role_id) references role (id)
 );
 
-select * from songs;
+select * from department;
+Select * from employee;
+
+Select subordinates.id
+,subordinates.first_name
+,subordinates.last_name
+, role.title as 'job title'
+, department.name as department
+, role.salary
+, concat(managers.first_name,' ', managers.last_name) as manager 
+from employee as subordinates
+left join employee as managers
+on subordinates.manager_id = managers.id
+inner join role
+on subordinates.role_id = role.id
+inner join department
+on department.id = role.department_id;
+
+-- Select subordinates.id
+-- ,subordinates.first_name
+-- ,subordinates.last_name
+-- , role.title as 'job title'
+-- , department.name as department
+-- , role.salary
+-- , concat(managers.first_name,' ', managers.last_name) as manager 
+-- from employee as subordinates
+-- left join role
+-- on subordinates.role_id = role.id
+-- where role.department_id = 1 
+-- inner join department
+-- on department.id = role.department_id
+-- left join employee as managers
+-- on subordinates.manager_id = managers.id;
+
+
+Select 
+subordinates.id
+,subordinates.first_name
+,subordinates.last_name
+ ,role.title as 'job title'
+, department.name as department
+, role.salary
+, concat(managers.first_name,' ', managers.last_name) as manager 
+from department 
+right join role 
+on role.department_id = department.id
+left join employee as subordinates
+on subordinates.role_id = role.id
+left join employee as managers
+on subordinates.manager_id = managers.id
+where department.id = 2
+order by salary desc;
+
+Select 
+subordinates.id
+,subordinates.first_name
+,subordinates.last_name
+ ,role.title as 'job title'
+, department.name as department
+, role.salary
+, concat(managers.first_name,' ', managers.last_name) as manager 
+from employee as managers
+right join employee as subordinates
+on subordinates.manager_id = managers.id
+left join role
+on subordinates.role_id = role.id
+left join department
+on department.id = role.department_id
+where managers.id = 4
+order by role.salary desc
+;
+
+
+select distinct concat(managers.first_name,' ', managers.last_name) as name from employee as subordinates
+left join employee as managers
+on managers.id = subordinates.manager_id;
+
+
+select distinct 
+  concat(managers.first_name,' ', managers.last_name) as name
+  , managers.id 
+  from employee as subordinates
+  left join employee as managers
+  on managers.id = subordinates.manager_id;
+
+
+select distinct 
+  concat(managers.first_name,' ', managers.last_name) as fullName
+  , managers.id
+  from employee as subordinates
+  inner join employee as managers
+  on managers.id = subordinates.manager_id
+  where subordinates.manager_id <> "" ;
